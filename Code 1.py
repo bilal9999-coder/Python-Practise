a9aca60144d069c3b5b527d9b179595e36ca8a18
Muhammad Bilal <bilalafnan13@gmail.com>
2026-08-06 16:39:57 +0500

    Complete Lec 1 Code

--- Code 1.py
+++ Code 1.py
@@ -1,4 +1,29 @@
+# Code 1
+
 n1 = int(input("Enter the first number: "))
 n2 = int(input("Enter the second number: "))
 sum = n1 + n2
 print("The sum is: ", sum)
+
+#Code 2
+
+s = float(input("Enter the side of the square: "))
+area = s ** 2
+print("The area of the square is: ", area)
+
+#Code 3
+
+n1 = float(input("Enter the first number: "))
+n2 = float(input("Enter the second number: "))
+avg = (n1 + n2) / 2
+print("The average is: ", avg)
+
+#Code 4
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+if(a >= b):
+    print(True)
+if(not(a >= b)):
+    print(False)
+    
