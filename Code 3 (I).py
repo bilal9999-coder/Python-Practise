089c2ffb1443d1b6234183f16f9fdadf7fe4b893
Muhammad Bilal <bilalafnan13@gmail.com>
2026-08-08 22:23:03 +0500

    Lecture 3 both codes done

---
+++ Code 3 (I).py
@@ -0,0 +1,13 @@
+# Task 1
+# a,b,c = input("Enter your 3 favourite movies: "). split(",")
+# lis = [a,b,c]
+# print(lis)
+
+# Task 2
+list = [1,"abc","abc",1]
+l1= list.copy() # As lis.copy return none so it was super necessory to copy the list and then reverse it
+l1.reverse
+if(list == l1):
+    print("True, It is palindrome")
+else:
+    print("false")
---
+++ Code 3(II).py
@@ -0,0 +1,19 @@
+# Task 3
+
+tuple = ("c","d","a","a","b","b","a")
+print(tuple.count("a"))
+
+# Task 4
+
+list = []
+list.append(tuple[0])
+list.append(tuple[1])
+list.append(tuple[2])
+list.append(tuple[3])
+list.append(tuple[4])
+list.append(tuple[5])
+list.append(tuple[6])
+list.sort()
+print (list)
+
+# List can be changes but tupple cannot , list is like you print list all the elements print no loopin it also has count, index , slicing etc
