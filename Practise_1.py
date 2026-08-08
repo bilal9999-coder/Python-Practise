f9c929472bec9b986b0230f955d054d642c43c98
Muhammad Bilal <bilalafnan13@gmail.com>
2026-08-08 21:35:57 +0500

    Updated: My Practise No 1

--- Practise_1.py
+++ Practise_1.py
@@ -6,4 +6,5 @@ if(str.endswith("old")):
 str1= str.capitalize()
 print(str1)
 print(str.find("b"))
+print(str[0:5]) # This is called string slicing and it print the index like 0 but not 5 
 print(str[3])
