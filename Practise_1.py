0b9755b64265ab6ca435022fb3ad4adcc7fc5163
Muhammad Bilal <bilalafnan13@gmail.com>
2026-08-08 21:28:14 +0500

    My Own Practise No 1

---
+++ Practise_1.py
@@ -0,0 +1,9 @@
+str = "i am bilal and i am 19 years old"
+if(str.startswith("i")):
+      print("Yes")
+if(str.endswith("old")):
+      print ("YES")
+str1= str.capitalize()
+print(str1)
+print(str.find("b"))
+print(str[3])
