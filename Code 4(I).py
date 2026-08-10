add1cf9caaf11965b0c953365b07f7bba42f1977
Muhammad Bilal <bilalafnan13@gmail.com>
2026-08-10 12:26:57 +0500

    Lecture 4 is done

---
+++ Code 4(I).py
@@ -0,0 +1,19 @@
+# Task 1
+
+dictionary = {
+    "table" : {"A piece of furniture", "list of facts and figures"},
+    "cat" : "A small animal"
+}
+
+print(dictionary)
+print(dictionary.keys())
+print(dictionary.values())
+print(dictionary.items())
+print(dictionary.get("cat"))
+
+
+# Task 2
+
+set = {"python","java", "C++", "python", "javascript", "java","python", "java", "C++", "C"}
+print(len(set))
+print(set)
---
+++ Code 4(II).py
@@ -0,0 +1,20 @@
+# Task 3
+
+# eng = int(input("Enter your marks in english: "))
+# urdu = int(input("Enter your marks in urdu: "))
+# maths = int(input("Enter your marks in maths: "))
+
+# dic= {}
+# dic["English"] = eng
+# dic["Urdu"] = urdu
+# dic.update({"Maths": maths})
+
+# print(dic)
+
+# Task 4
+
+set = set()
+set.add(9)
+set.add('9.0')
+
+print(set)
