49f962ff9151945a231275aa339ec01027596ccb
Muhammad Bilal <bilalafnan13@gmail.com>
2026-08-10 17:34:58 +0500

    Lecture 5 codes done (Sorry two files were lost)

---
+++ Code 5(I).py
@@ -0,0 +1,14 @@
+# Task 1
+
+for num in range(1,101):
+    print (num)
+
+# Task 2
+
+for num in range(100,0,-1):
+    print (num)
+
+# Task 3
+num = int (input("Enter the number whose table you want: "))
+for n in range (1,11):
+    print(num,"x",n,"=",n*num)
---
+++ Code 5(II).py
@@ -0,0 +1,21 @@
+# Task 1
+
+# n = 5
+# i =0
+# sum =0
+# while i<= 5:
+#     sum = sum + i
+#     i+=1
+
+print (sum)
+
+
+# Task 2
+
+fac = 1
+n= 5
+
+for num in range(1,n+1):
+    fac = fac * num
+
+print(fac)
